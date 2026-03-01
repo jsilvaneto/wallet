5f8e616c56016466c04fdadec2872d58efcef87c
Jean Neto | PrimeDB <jean.neto@primedb.solutions>
2026-03-01 13:17:48 -0300

    feat: set up initial NestJS API project with core dependencies and Swagger documentation.

--- api/dist/main.js
+++ api/dist/main.js
@@ -5,11 +5,20 @@ const app_module_1 = require("./app.module");
 const global_exception_filter_1 = require("./presentation/filters/global-exception.filter");
 const logging_interceptor_1 = require("./presentation/interceptors/logging.interceptor");
 const common_1 = require("@nestjs/common");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true, transform: true }));
     app.useGlobalFilters(new global_exception_filter_1.GlobalExceptionFilter());
     app.useGlobalInterceptors(new logging_interceptor_1.LoggingInterceptor());
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('Wallet API')
+        .setDescription('The Wallet API description')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    const documentFactory = () => swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, documentFactory);
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- api/src/main.ts
+++ api/src/main.ts
@@ -4,12 +4,23 @@ import { GlobalExceptionFilter } from './presentation/filters/global-exception.f
 import { LoggingInterceptor } from './presentation/interceptors/logging.interceptor';
 import { ValidationPipe } from '@nestjs/common';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalInterceptors(new LoggingInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Wallet API')
+    .setDescription('The Wallet API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
